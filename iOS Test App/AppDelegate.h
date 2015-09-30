780425dca4343cd94fb02601ff66e478d01d339d
artoyer <arto.yeritsyan@gmail.com>
2015-09-30 17:54:45 +0400

    Initial Commit

---
+++ iOS Test App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS Test App
+//
+//  Created by Artavazd Yeritsyan on 9/30/15.
+//  Copyright © 2015 Artavazd Yeritsyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS Test App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS Test App
+//
+//  Created by Artavazd Yeritsyan on 9/30/15.
+//  Copyright © 2015 Artavazd Yeritsyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
